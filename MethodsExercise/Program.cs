5d8aef16c841ca7b95cfa6fb302b5700d6a70374
Elsa Villarreal <elsa22@att.net>
2024-05-19 14:52:58 -0500

    initial commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace MethodsExercise
+﻿
+using System.Drawing;
+
+namespace MethodsExercise
 {
     public class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            var  name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+
+            Console.WriteLine($"Hey, {name}! I saw a {color} {animal} wearing a {band} concert shirt.");
+
         }
+
+        
     }
 }
