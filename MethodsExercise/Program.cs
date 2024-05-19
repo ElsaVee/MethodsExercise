bcf89f5c85bd7f4eef255b99c9e3b81b6507d3d3
Elsa Villarreal <elsa22@att.net>
2024-05-19 17:13:41 -0500

    added exercise 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel;
 using System.Drawing;
 
 namespace MethodsExercise
@@ -6,24 +7,68 @@ namespace MethodsExercise
     public class Program
     {
 
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+           //Exercise 1
+            
             Console.WriteLine("What is your name?");
-            var  name = Console.ReadLine();
+            var name = Console.ReadLine();
 
             Console.WriteLine("What is your favorite color?");
             var color = Console.ReadLine();
 
             Console.WriteLine("What is your favorite animal?");
-            string animal = Console.ReadLine();
+            var animal = Console.ReadLine();
 
             Console.WriteLine("What is your favorite band?");
-            string band = Console.ReadLine();
+            var band = Console.ReadLine();
 
             Console.WriteLine($"Hey, {name}! I saw a {color} {animal} wearing a {band} concert shirt.");
 
+            //Exercise 2
+
+            var addition = Add(2, 7);
+            var subtration = Subtract(19, 9);
+            var multiplication = Multiply(3, 7);
+            var division = Divide(18,9);
+            var mod = Modulus(3, 2);
+
+            Console.WriteLine();
+            Console.WriteLine($"Addition {addition}, Subtration {subtration}, Multiplication {multiplication}, Divide {division}, Modulus {mod}");
+            
         }
 
-        
+      // Methods
+        public static int Add(int x, int y)
+        {
+            
+            return x + y;
+        }
+
+        public static int Subtract(int x, int y)
+        {
+
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+
+            return x * y;
+        }
+
+        public static int Divide(int x, int y)
+        {
+
+            return x / y;
+        }
+
+        public static int Modulus(int x, int y)
+        {
+
+            return x % y;
+        }
+
+
     }
 }
--- MethodsExercise/obj/Debug/net6.0/MethodsExercise.AssemblyInfo.cs
+++ MethodsExercise/obj/Debug/net6.0/MethodsExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472d02f9d75aac5a20b813b93fd9e5787f6c5186")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8aef16c841ca7b95cfa6fb302b5700d6a70374")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
